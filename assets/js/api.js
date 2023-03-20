75a3b9db18762ed71eb7997b4848e95e993cee71
Lara costa <larasmila64@gmail.com>
2023-03-20 15:01:32 -0300

    update json informations

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
-async function  fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/LaraCosta66/js-developer-portfolio/main/data/profile.json'
-   const fetching = await fetch(url)
-   return await fetching.json()
-   
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/LaraCosta66/LaraCosta.github.io/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
